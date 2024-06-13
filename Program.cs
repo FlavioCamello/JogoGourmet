65b9e7c5c37553d9e6d77d86eebed03ed76a7a18
flavio <flavio@DESKTOP-ITRUDDG>
2024-06-13 09:09:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+    //GameStore gameStore = new GameStore();
+    //var root = gameStore.StartGame();
+
+    //while (true)
+    //{
+    //    Console.WriteLine("Pense em um prato que goste:");
+    //    Console.WriteLine();
+    //    var answer = gameStore.GetAnswer("Digite qualquer tecla para continuar ou Q para encerrar");
+    //    if (answer.Equals("Q"))
+    //    {
+    //        break;
+    //    }
+    //    else
+    //    {
+    //        gameStore.LookUp(root);
+    //    }
+    //}
+    Console.WriteLine("Programa encerrado!");
+
